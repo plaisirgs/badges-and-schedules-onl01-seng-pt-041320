b5d884643aadb0b916d0b84b45176ac99dca4af5
GINELLE PLAISIR <plaisirginelle@gmail.com>
2020-04-21 11:49:05 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -8,7 +8,7 @@ end
 
 def assign_rooms(speakers)
   new_arr = [ ]
-  speakers.each_with_index do |name, index|
+  speakers.map_with_index do |name, index|
   new_arr << "Hello, #{name}! You'll be assigned to room #{index + 1}!"
   end
 end
