95ed87b33545d8c9e82b4dc680d561c95bc39ae2
GINELLE PLAISIR <plaisirginelle@gmail.com>
2020-04-21 11:42:41 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,11 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+  names.map {|name| "Hello, my name is #{name}."}
+end
+
+def assign_rooms(speakers)
+  "Hello, #{speakers}!" You'll be assigned to room 
+end
