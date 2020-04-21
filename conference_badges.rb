5d8ff326fde2a0e286cc9a6cc969c4449d8ec21b
GINELLE PLAISIR <plaisirginelle@gmail.com>
2020-04-21 12:02:42 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -15,7 +15,7 @@ def assign_rooms(speakers)
 end
 
 def printer(attendees)
-  batch_badge_creator(attendees).each do |badge|
-    puts badge
+  batch_badge_creator(attendees).each {|badge| puts badge}
+  assign_room(attendees).each {|assign| puts assign}
   end
 end
